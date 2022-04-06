a2e2662f50ed335411ffff9dc94c8bb4c0ecff8a
amIToan <toandz4101998@gmail.com>
2022-04-06 17:27:10 +0700

    pushing afternoon

---
+++ data/Products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    name: "Velcro Ballerinas For Girls  (Pink)",
+    image: "/images/6.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    categories: ["Nogi", "BJJ"],
+    size: ["A0", "A1"],
+    color: ["Red", "Yellow", "Purple"],
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Velcro Sneakers For Boys & Girls  (Blue)",
+    image: "/images/5.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    categories: ["Gi", "BJJ"],
+    size: ["A0", "A1", "A3", "A2", "A5"],
+    color: ["Pink", "Grey", "Purple"],
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Sesame Street Unisex-Child ELMO Puppet Slipper",
+    image: "/images/4.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    categories: ["Gi", "BJJ"],
+    size: ["A3", "A2", "A5"],
+    color: ["Orange", "Tune"],
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "Lace Casual Boots For Boys & Girls  (Tan)",
+    image: "/images/3.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    categories: ["Gi", "BJJ", "Pants"],
+    size: ["A0",],
+    color: ["White"],
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "Lace Walking Shoes For Boys & Girls  (Pink)",
+    image: "/images/2.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    categories: ["Gi", "BJJ", "Pants"],
+    size: ["A0", "A7", "A9"],
+    color: ["Blue", "White"],
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Women Red Heels Sandal",
+    image: "/images/1.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    categories: ["BJJ", "MMA T-shirt"],
+    size: ["A0", "A7", "A9"],
+    color: ["Black", "Red"],
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
